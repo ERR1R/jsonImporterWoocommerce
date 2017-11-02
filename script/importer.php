c6165d875089bb2e860f5065aa7dccb11b3674fb
Daniel <dbogarin88@gmail.com>
2017-11-02 10:48:25 -0600

    this removes some dummy lines

---
+++ script/importer.php
@@ -0,0 +1,216 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Automattic\WooCommerce\Client;
+use Automattic\WooCommerce\HttpClient\HttpClientException;
+
+
+function getWoocommerceConfig()
+{
+
+    $woocommerce = new Client(
+        'https://www.dbogarin.com',
+        'ck_5750cdf705b96742cc4446f4e86d355f5575c90f',
+        'cs_d1980454e276a9275f2ff5c4fd2bb78f842f01bf',
+        [
+            'wp_api' => true,
+            'version' => 'wc/v2',
+            'query_string_auth' => false,
+        ]
+    );
+
+    return $woocommerce;
+}
+
+/**
+ * Parse JSON file.
+ *
+ * @param  string $file
+ * @return array
+ */
+function getJsonFromFile()
+{
+    $file = '../db/productos.json';
+    $json = json_decode(file_get_contents($file), true);
+    return $json;
+}
+
+//////////
+
+
+function createProducts()
+{
+    $woocommerce = getWoocommerceConfig();
+    $products = getJsonFromFile();
+
+    $imgCounter = 0;
+    foreach ($products as $product)
+        {
+		/*Chec sku before create the product */
+
+        $imagesFormated = array();
+	    /*Main information */
+        $name = $product['titulo'];
+        $slug = $product['url'];
+        $sku = $product['sku'];
+        $description = $product['desc'];
+        $images = $product['pics'];
+        $articulos = $product['articulos'];
+        $categories = $product['categorias'];
+        $categoriesIds = array();
+        foreach ($images as $image)
+            {
+            $imagesFormated[] = [
+                'src' => $image,
+                'position' => 0
+            ]; /* TODO: FIX POSITON */
+            $imgCounter++;
+        } 
+
+
+		/* Prepare categories */
+        foreach ($categories as $category)
+            {
+            $categoriesIds[] = ['id' => getCategoryIdByName($category)];
+        }
+        $finalProduct = [
+            'name' => $name,
+            'slug' => $slug,
+            'sku' => $sku,
+            'description' => $description,
+            'images' => $imagesFormated,
+            'categories' => $categoriesIds,
+            'attributes' => getproductAtributesNames($articulos)
+
+        ];
+
+        $productResult = $woocommerce->post('products', $finalProduct);
+
+        return $productResult;
+    }
+}
+
+
+function createCategories()
+{
+    $categoryValues = getCategories();
+    $woocommerce = getWoocommerceConfig();
+	
+	/* Add a category verificator */
+    foreach ($categoryValues as $value)
+        {
+        if (!checkCategoryByname($value))
+            {
+            $data = [
+                'name' => $value
+            ];
+            print_r($woocommerce->post('products/categories', $data));
+        }
+    }
+}
+
+function checkCategoryByName($categoryName)
+{
+    $woocommerce = getWoocommerceConfig();
+    $categories = $woocommerce->get('products/categories');
+    foreach ($categories as $category)
+        {
+        if ($category['name'] === $categoryName)
+            {
+            return true;
+        }
+    }
+    return false;
+}
+
+/** CATEGORIES  **/
+function getCategories()
+{
+    $products = getJsonFromFile();
+    $categories = array_column($products, 'categorias');
+
+    foreach ($categories as $categoryItems)
+        {
+        foreach ($categoryItems as $categoryValue)
+            {
+            $categoryPlainValues[] = $categoryValue;
+
+        }
+    }
+    $categoryList = array_unique($categoryPlainValues);
+    return $categoryList;
+}
+
+
+function getCategoryIdByName($categoryName)
+{
+    $woocommerce = getWoocommerceConfig();
+    $categories = $woocommerce->get('products/categories');
+    foreach ($categories as $category)
+        {
+        if ($category['name'] == $categoryName)
+            {
+            return $category['id'];
+        }
+    }
+}
+
+function getproductAtributesNames($articulos)
+{
+    $keys = array();
+    foreach ($articulos as $articulo)
+        {
+        $terms = $articulo['config'];
+        foreach ($terms as $key => $term)
+            {
+            array_push($keys, $key);
+        }
+    }
+       /* remove repeted keys*/
+    $keys = array_unique($keys);
+    $configlist = array_column($articulos, 'config');
+    $options = array();
+    foreach ($keys as $key)
+        {
+        $attributes = array(
+            array(
+                'name' => $key,
+                'slug' => 'attr_' . $key,
+                'visible' => true,
+                'variation' => true,
+                'options' => getTermsByKeyName($key, $configlist)
+            )
+        );
+    }
+    return $attributes;
+}
+
+function getTermsByKeyName($keyName, $configList)
+{
+	//var_dump($configList);
+    $options = array();
+    foreach ($configList as $config)
+        {
+        foreach ($config as $key => $term)
+            {
+            if ($key == $keyName)
+                {
+                array_push($options, $term);
+            }
+        }
+    }
+    return $options;
+}
+
+function prepareInitialConfig()
+{
+    echo ('Importing data, wait...');
+    createCategories();
+    createProducts();
+    echo ('Done!');
+}
+
+prepareInitialConfig();
+
+
+?>
